64289fd82603c3814fc435f3607360e3d70b943e
Matt Gale <matt@littleball.co.uk>
2014-05-16 16:39:15 +0100

    added fix for hidden files

--- slushfile.js
+++ slushfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
   install = require('gulp-install'),
   conflict = require('gulp-conflict'),
   template = require('gulp-template'),
+  rename = require('gulp-rename'),
   inquirer = require('inquirer');
 
 gulp.task('default', function (done) {
@@ -38,6 +39,11 @@ gulp.task('default', function (done) {
 
     gulp.src(__dirname + '/templates/**')
       .pipe(template(answers))
+      .pipe(rename(function (file) {
+        if (file.basename[0] === '_') {
+          file.basename = '.' + file.basename.slice(1);
+        }
+      }))
       .pipe(conflict('./'))
       .pipe(gulp.dest('./'))
       .pipe(install())
