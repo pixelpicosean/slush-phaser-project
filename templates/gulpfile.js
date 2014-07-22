bb6f03e656dad8be63a676644b424888c78fab9b
Sean <pixelpicosean@gmail.com>
2014-07-22 10:42:50 +0800

    Use processhtml to handle html file and remove karma test stuff

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    browserify = require('browserify'),
-    imagemin = require('gulp-imagemin'),
-    browserSync = require('browser-sync'),
-    sass = require('gulp-sass'),
-    source = require('vinyl-source-stream'),
-    karma = require('gulp-karma')/*,
-    streamify = require('gulp-streamify'),
-    uglify = require('gulp-uglify')*/;
+  processhtml = require('gulp-processhtml'),
+  jshint = require('gulp-jshint'),
+  browserify = require('browserify'),
+  imagemin = require('gulp-imagemin'),
+  browserSync = require('browser-sync'),
+  sass = require('gulp-sass'),
+  source = require('vinyl-source-stream'),/*,
+  streamify = require('gulp-streamify'),
+  uglify = require('gulp-uglify')*/;
 
 gulp.task('default', ['compile', 'watch', 'server']);
 
@@ -24,19 +24,7 @@ gulp.task('script-hints', function () {
     });
 });
 
-gulp.task('script-test', function () {
-  return gulp.src('src/**/*_spec.js')
-    .pipe(karma({
-      baseDir: './',
-      configFile: 'karma.config.js',
-      action: 'run'
-    }))
-    .on('error', function (err) {
-      throw err;
-    });
-});
-
-gulp.task('script-compile', ['script-hints', 'script-test'], function () {
+gulp.task('script-compile', ['script-hints'], function () {
   var bundleStream = browserify('./src/js/base.js').bundle();
 
   bundleStream
@@ -51,17 +39,29 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('bin/css'));
 });
 
+gulp.task('processhtml', function() {
+  return gulp.src('src/index.html')
+    .pipe(processhtml('index.html'))
+    .pipe(gulp.dest('bin'));
+})
+
 gulp.task('assets', function () {
   return gulp.src(['src/assets/*.png', 'src/assets/*.jpg'])
     .pipe(imagemin())
     .pipe(gulp.dest('bin/assets'));
 });
 
-gulp.task('watch', ['watch-scripts']);
+gulp.task('watch', ['watch-scripts', 'watch-html']);
 
 gulp.task('watch-scripts', function () {
   return gulp.watch('src/js/**/*.js', function () {
     gulp.run('scripts');
+  });
+});
+
+gulp.task('watch-html', function () {
+  return gulp.watch('src/index.html', function () {
+    gulp.run('processhtml');
   });
 });
 
