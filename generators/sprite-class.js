9024b916c696973e83328706db21a146aee01f61
Sean <pixelpicosean@gmail.com>
2014-11-14 22:42:44 +0800

    Remove deprecated templates and rename "scene" to "state"

--- generators/sprite-class.js
+++
@@ -1,8 +0,0 @@
-export default Phaser.Sprite.extend({
-    init: function(game, x, y) {
-        this._super(game, x, y);
-
-        /* init code here */
-        this.anchor.setTo(0.5);
-    }
-});
