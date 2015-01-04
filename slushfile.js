01fb614a2bc530d2bf7a6766ecee2f3232606a5b
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-01-04 14:02:01 -0200

    Refactoring generator loading, load game project config

--- slushfile.js
+++ slushfile.js
@@ -1,14 +1,17 @@
-var gulp       = require('gulp');
-var plugins    = require('gulp-load-plugins')();
-var generators = require('require-dir')('./generators');
+var bootstrap = require('./utils/bootstrap');
 
-var dependencies = {
-    'inquirer' : require('inquirer')
-};
+var gulp          = require('gulp');
+var plugins       = require('gulp-load-plugins')();
+var generators    = require('require-dir')('./generators');
+var dependencies  = bootstrap.loadDependencies([
+    'inquirer',
+    'underscore.string'
+]);
+var projectConfig = bootstrap.loadProjectConfig('project-config.json');
 
 Object.keys(generators)
     .map(function (key) { return generators[key] })
     .filter(function (obj) { return typeof obj === 'function' })
     .forEach(function (generator) {
-        generator(gulp, plugins, dependencies)
+        generator(gulp, plugins, dependencies, projectConfig)
     });
