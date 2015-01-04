945755aa5653b0a213c9cbea84bc88d1aa2e2b86
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-01-04 11:24:16 -0200

    Move out bower components directory from `static`

--- templates/default/gulp/tasks/dist.js
+++ templates/default/gulp/tasks/dist.js
@@ -41,12 +41,7 @@ module.exports = function (gulp, $, config, deps) {
     });
 
     gulp.task('dist:assets', function () {
-        gulp.src([
-            'static/**',
-            '!static/bower_components',     // Workaround to ensure both directory
-            '!static/bower_components/**'   // and its contents don't get copied.
-        ])
-            .pipe(handleErrors())
+        gulp.src(globs['assets'])
             .pipe(gulp.dest(dirs['product']));
     });
 
